ed812f4bd9cace86e69e12ad180b88f8f39bc9e0
CNflysky <cnflysky@qq.com>
2021-07-30 07:43:03 +0800

    fixed cpu usage bug

--- src/main.c
+++ src/main.c
@@ -22,7 +22,7 @@ void cpu_mem_temp_info_page() // takes about 1s
 {
     ssd1315_clear_buf();
     ssd1315_draw_rectangle(0, 0, 127, 63, false);
-    ssd1315_draw_string(1, 1, "unifont_16s", get_load(999));
+    ssd1315_draw_string(1, 1, "unifont_16", get_load(999));
     ssd1315_draw_string(1, 17, "unifont_16", get_mem());
     ssd1315_draw_string(1, 32, "unifont_16", get_boot_time());
     ssd1315_draw_string(1, 47, "unifont_16", get_core_temp());
@@ -51,7 +51,7 @@ int main()
     printf("Initializing OLED...\n");
     ssd1315_init();
     printf("Starting loop...\n");
-    uint8_t i = 0;
+            uint8_t i = 0;
     while (1)
     {
         do
